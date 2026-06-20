7680d2fe951c69de8c46ae466e129d233285adcb
Alibegovic Hamdija <ali964ham@gmail.com>
2026-06-20 20:00:03 +0200

    Project has finished

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const p = document.querySelector("p");
 function updateClock(){
     const currentDate = new Date();
     setTimeout(updateClock, 1000);
+    const hour = currentDate.getHours();
+    const minute = currentDate.getMinutes();
+    const second = currentDate.getSeconds();
+    const hourDeg = (hour / 12) * 360;
+    h.style.transform = `rotate(${hourDeg}deg)`;
+    const minuteDeg = (minute / 60) * 360;
+    m.style.transform = `rotate(${minuteDeg}deg)`;
+    const secondDeg = (second / 60) * 360;
+    s.style.transform = `rotate(${secondDeg}deg)`;
     p.innerText = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`;
 }
 updateClock();
